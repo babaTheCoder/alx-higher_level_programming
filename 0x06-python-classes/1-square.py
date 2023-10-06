61f379732cdff0ebbea3ec3b30018987c658970e
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-06 02:51:24 +0100

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module defines a square"""
+
+
+class Square:
+    """Square: an empty class that defines square
+       Nothing happens here
+    """
+
+    def __init__(self, size):
+        """This initializes the class when an instance is first created
+
+        Args:
+            size (int): dimensions for the square
+        """
+        self.__size = size
