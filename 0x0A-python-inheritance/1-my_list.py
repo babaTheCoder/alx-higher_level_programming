ec0c79418f1afe78f806966ee322776bea65b2ea
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-14 20:59:34 +0100

    1. My List

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,19 @@
 
 class MyList(list):
     """This class returns the elements of a list in a sorted order"""
+
+    def custom_sort(lst):
+        """This functions sorts a list"""
+
+        n = len(lst)
+        new_lst = lst[:]
+        for i in range(n - 1):
+            for j in range(n - 1 - i):
+                if new_lst[j] > new_lst[j + 1]:
+                    new_lst[j], new_lst[j + 1] =  new_lst[j + 1], new_lst[j]
+        return new_lst
+
     def print_sorted(self):
-        new_list = sorted(self)
-        print(new_list)
+        """Prints out a sorted list"""
+        sort_list = MyList.custom_sort(self)
+        print(sort_list)
