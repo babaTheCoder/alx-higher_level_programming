367cfaf6c348921e6741c59bf9371c8a5411338f
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-15 04:29:06 +0100

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module checks if an object is same kind as a given class"""
+
+def is_kind_of_class(obj, a_class):
+    """Checks using issinstance
+
+    Args:
+        obj: object to check
+        a_class: Class to check against
+
+    Returns:
+        True if obj is isinstance of a_class or False
+    """
+
+    return isinstance(obj, a_class)
