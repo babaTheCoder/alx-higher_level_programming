2905a6a8cb09081ad741740c1fb18b4f40d065d8
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-15 08:22:28 +0100

    Removed area function in Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,6 +10,3 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
-    def area(self):
-        return self.__width * self.__height
