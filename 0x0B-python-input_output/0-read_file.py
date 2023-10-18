6a727f06995777602d5f6467926bc19663f46a30
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-19 00:16:35 +0100

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module reads a file and print its content to standard output"""
+
+
+def read_file(filename=""):
+    """This functions reads the content of a file
+
+    Args:
+        filename (str): The name of the file to open
+    """
+
+    with open(filename, encoding="utf-8") as currentfile:
+        print(currentfile.read())
