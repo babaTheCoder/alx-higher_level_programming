4245dc03345179aba794a94d8ce3f340b815cf20
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-19 19:33:49 +0000

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Creates the Pascal's triangle"""
+
+def pascal_triangle(n):
+    """A function that generates the pascal's triangle
+	
+    Args:
+        n (int): the number to use for printing
+
+    Returns:
+        A trinagle of lists
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            trinagle.append([1])
+        else:
+            previous_row = triangle[-1]
+            new_row = [1]
+
+            for j in range(1, i):
+                new_row.append(previous_row[j - 1] + previous_row[j])
+            new_row.append(1)
+            traingle.append(new_row)
+    retrn triangle    
