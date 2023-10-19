1687c12000b5d2a0aea71f973172f92a318bb973
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-19 02:14:58 +0100

    7. Load, add, save V.2

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 arguments = sys.argv[1:]
+data = []
 
-data = load_from_json_file("add_item.json")
-data.extend(arguments)
-save_to_json_file(data, "add_item.json")
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError as e:
+    new_file = open("add_item.json", mode="w", encoding="utf-8")
+    new_file.close()
+finally:
+    data.extend(arguments)
+    save_to_json_file(data, "add_item.json")
