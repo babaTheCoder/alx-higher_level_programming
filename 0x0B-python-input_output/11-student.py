09adee1b5e3888244fdc0db24d01a87aa29e47b7
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-10-19 03:14:46 +0100

    11. student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Defines a student"""
+
+
+class Student:
+    """A student class
+
+    Attrbs:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student
+
+    Returns:
+        a dictionary representation of the student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Create a dictionary representation of Student instance"""
+        if attrs is None:
+            return self.__dict__
+        return {at: getattr(self, at) for at in attrs if hasattr(self, at)}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
