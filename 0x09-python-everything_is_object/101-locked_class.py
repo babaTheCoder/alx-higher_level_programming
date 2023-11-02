0b6dc6112a8eefacfeeeceed7f9bcbbcea2fa433
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-11-02 13:13:50 +0000

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-class LockedClass:
-    __slots__ = ['first_name']
-
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError(f"Cannot create a new instance attribute '{name}'")
-        self.__dict__(name) = value
