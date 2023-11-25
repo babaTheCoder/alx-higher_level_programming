2ac96cd690d80a37d78177c81a45b29c38c9e549
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-11-25 20:00:55 +0000

    Created a function [add_integer()]
    - Prototype: def add_integer(a, b=98):
    - a and b must be integers or floats, otherwise raise
    - a TypeError exception with the message a must be an
    - integer or b must be an integer
    - a and b must be first casted to integers if they are float
    - Returns an integer: the addition of a and b
    
    Wrote tests in ./tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,29 +1,25 @@
-#!usr/bin/python
+#!/usr/bin/python3
 
-"""This module takes two values and return their sum"""
+"""This module adds two numbers"""
 
 
 def add_integer(a, b=98):
-    """Adds two values or integers
+    """This function takes two values and return their sum
 
     Args:
-        a (int): First value for addition
-        b (int): second value
+        a (int, float): first addend
+        b (int, float): second addend
 
     Returns:
-        value of summation of a and b
+        sum or a and b
     """
-
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
     if isinstance(a, float):
         a = int(a)
-
     if isinstance(b, float):
-        a = int(b)
+        b = int(b)
 
-    return (a+b)
+    return a + b
