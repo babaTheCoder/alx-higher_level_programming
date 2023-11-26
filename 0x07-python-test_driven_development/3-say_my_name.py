e2707681c905b637ef9f4357e8ab3fa266ec9281
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-11-26 13:01:07 +0000

    2. Say my name
    Write a function that prints My name is <first name> <last name>
    
    - Prototype: def say_my_name(first_name, last_name=""):
    - first_name and last_name must be strings otherwise,
    - raise a TypeError exception with the message first_name
    - must be a string or last_name must be a string
    
    Test can be found in ./tests/3-say_my_name.txt

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module prints out a user name"""
+
+def say_my_name(first_name, last_name=""):
+    """Prints out name
+
+    Args:
+        first_name (str): first name to print
+        last_name (str): last name of person
+
+    Return:
+        nothing, prints out full name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
