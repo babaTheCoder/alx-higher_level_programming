5dfbf79d6a4d2a3bbfeb62f508d9dab33a273ec0
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-12-05 12:38:30 +0000

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    This module defines a function
+    that writes a given string to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a given file
+
+        Args:
+            filename: path to file
+            text (str): string to write to file
+
+        Returns:
+            nothing
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
