682821cca14e4aeb22604480b9838d22748db4b7
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-12-05 08:55:22 +0000

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""prints lists inherited
+"""
 
 class MyList(list):
+    """child class of list
+    """
     def print_sorted(self):
-        print(sorted(self))
+        """
+        Module to sort a list
+        """
+        print(sorted(self.copy()))
